9d2f10a54364b5b793404efc9f0b305b3abebba9
Eric Mossotti <ecmossotti@gmail.com>
2024-03-25 04:11:29 -0500

    Added messaging prompts that now work.

--- duckDrops.R
+++ duckDrops.R
@@ -1,21 +1,25 @@
 # For easily dropping specified tables from the duck db. 
 
-paths <- duckdb::dbListTables(dbconn_fltrd)
+paths <- duckdb::dbListTables(dbconn)
 
 drops_tables <- function(path) {
-    duckdb::dbRemoveTable(dbconn_fltrd, 
+    duckdb::dbRemoveTable(dbconn, 
                           path)
-}
+    }
 
 choice <- c()
 
 cat("Remove which tables? (separate by comma)\n")
 for (i in seq(paths)) {
-    choice[i] <- readline(prompt = message(as.character(i),
-                                           ". ",
-                                           paths[i]))
-}
+    message(as.character(i), ". ", paths[i])
+} 
 
+choice <- readline(prompt = message("Choose\n")) |>
+    stringr::str_split(",") |>
+    unlist() |>
+    as.integer()
+    
 paths[choice] |> purrr::walk(drops_tables)
 
-duckdb::dbListTables(dbconn_fltrd)
+message("Tables left:\n",
+        duckdb::dbListTables(dbconn))
