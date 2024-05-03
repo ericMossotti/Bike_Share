8b063948c28e57804564500c2fd7d0b7f04150b5
Eric Mossotti <ecmossotti@gmail.com>
2024-05-02 19:14:09 -0500

    Add mapViewer()

---
+++ mapViewer.R
@@ -0,0 +1,25 @@
+# I needed the stations groups' burst buttons to fit
+# the viewing window in my document and the only way I could think of is to 
+#   split the stations into multiple maps. This reduces duplicate code.
+
+
+
+mapViewer <- function(x) {
+    
+    nameSlice <- sampled_stations |>
+        dplyr::ungroup() |>
+        dplyr::distinct(station_names) |>
+        dplyr::slice(x)
+    
+    viewMap <- sampled_stations |>
+        dplyr::filter(station_names %in% nameSlice$station_names) |>
+        sf::st_as_sf(coords = c(3:2), crs = 4326) |>
+        mapview::mapview(
+            zcol = "station_names",
+            col.regions = randomColors,
+            map.types = "OpenStreetMap",
+            burst = TRUE,
+            legend = FALSE)
+    
+    return(viewMap)
+}
