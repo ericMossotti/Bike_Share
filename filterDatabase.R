622937aed77df646ed2674c8550cd1bf9b1d438e
Eric Mossotti <ecmossotti@gmail.com>
2024-05-13 05:36:12 -0500

    Update to format

--- filterDatabase.R
+++ filterDatabase.R
@@ -1,12 +1,8 @@
 filterDatabase <- function(conxn = dbconn,
                            path1 = dupelessPath,
                            path2 = tblPath_fltrd) {
-    dplyr::tbl(conxn,
-               path1,
-               check_from = FALSE) |>
-        dplyr::filter(trip_time > 1,
-                      trip_time < 480,
-                      rideable_type != "docked_bike") |>
+    dplyr::tbl(conxn, path1, check_from = FALSE) |>
+        dplyr::filter(trip_time > 1, trip_time < 480, rideable_type != "docked_bike") |>
         dplyr::collect() |>
         # Might as well calculate distance traveled while at it.
         dplyr::mutate(
@@ -17,11 +13,9 @@ filterDatabase <- function(conxn = dbconn,
             mph = (miles / (trip_time / 60))
         ) |>
         # It's nonsensical to rent a bike for distances easily walked.
-        dplyr::filter(miles > 0.1,
-                      # Seems that pro cyclists average around 20 mph,
+        dplyr::filter(miles > 0.1, # Seems that pro cyclists average around 20 mph,
                       # so I set that as the ceiling.
-                      mph <= 20,
-                      # To account for time spent idling, stoplights and
+                      mph <= 20, # To account for time spent idling, stoplights and
                       # traffic.
                       mph > 1) |>
         duckdb::dbWriteTable(
