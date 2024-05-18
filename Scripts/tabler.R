1873c79bf542abbcec93a6c280070fadd843ec1c
Eric Mossotti <ecmossotti@gmail.com>
2024-05-18 09:31:11 -0500

    Add tbl_summary functionality and handle indirect variable value reference

--- Scripts/tabler.R
+++ Scripts/tabler.R
@@ -5,13 +5,17 @@ tabler <- function(tbl_name,
                    title = NULL,
                    subtitle = NULL,
                    groupName = NULL,
+                   by = NULL,
+                   label = NULL,
                    value_columns = gt::everything(),
                    decimals = 0,
                    font_color = "SeaShell",
                    bg_color = "transparent",
                    vline_color = "gray20",
                    hline_color = "gray20",
-                   hide_column_labels = FALSE) {
+                   hide_column_labels = FALSE,
+                   isSummary = FALSE) {
+    # Need to account for grouped tables and non-grouped tables
     if (!is.null(groupName)) {
         tbl <- gt::gt(tbl_name,
                       groupname_col = groupName,
@@ -27,8 +31,25 @@ tabler <- function(tbl_name,
             ) |>
             gt::tab_source_note(source_note = source_note)
         
+    } 
+    
+    else if (isSummary == TRUE) {
+        tbl <- tbl_name |>
+            gtsummary::tbl_summary(by = {{ by }}, label = label) |>
+            gtsummary::add_p() |>
+            gtsummary::as_gt() |>
+            gt::tab_options(
+                table.background.color = bg_color,
+                table.font.color = font_color,
+                table_body.vlines.color = vline_color,
+                table_body.hlines.color = hline_color,
+                column_labels.hidden = hide_column_labels
+            ) |>
+            gt::tab_header(title = title) |>
+            gt::tab_source_note(source_note = source_note)
         
     } else {
+        
         tbl <- gt::gt(tbl_name) |>
             gt::tab_header(title = title) |>
             gt::fmt_number(decimals = decimals, columns = value_columns) |>
@@ -43,11 +64,5 @@ tabler <- function(tbl_name,
         
     }
     
-    
     return(tbl)
 }
-
-
-# tblGrouper ----
-# 
-# 
