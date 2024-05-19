230f7bc6ce213c05d85df240e75df934222f2292
Eric Mossotti <ecmossotti@gmail.com>
2024-05-18 19:09:20 -0500

    Fix bugs, now works for everything tried so far.

--- Scripts/tabler.R
+++ Scripts/tabler.R
@@ -7,6 +7,12 @@ tabler <- function(tbl_name,
                    groupName = NULL,
                    by = NULL,
                    label = NULL,
+                   label_n = "N",
+                   label_member = "member_casual",
+                   colVar = as.character(colVar),
+                   colValue,
+                   location = location,
+                   footnote = footnote,
                    value_columns = gt::everything(),
                    decimals = 0,
                    font_color = "SeaShell",
@@ -14,8 +20,12 @@ tabler <- function(tbl_name,
                    vline_color = "gray20",
                    hline_color = "gray20",
                    hide_column_labels = FALSE,
-                   isSummary = FALSE) {
+                   isSummary = FALSE,
+                   isBinary = FALSE) {
+    
     # Need to account for grouped tables and non-grouped tables
+    
+    # Grouped ----
     if (!is.null(groupName)) {
         tbl <- gt::gt(tbl_name,
                       groupname_col = groupName,
@@ -29,10 +39,14 @@ tabler <- function(tbl_name,
                 table_body.hlines.color = hline_color,
                 column_labels.hidden = hide_column_labels,
             ) |>
-            gt::tab_source_note(source_note = source_note)
-        
+            gt::cols_label(n = label_n, 
+                           member_casual = label_member) |>
+            gt::tab_footnote(footnote = {{ footnote }},
+                             location = gt::cells_column_labels(columns = {{ location }})) |>
+            gt::tab_source_note(source_note = {{ source_note }})
     } 
     
+    # Chi Square ----
     else if (isSummary == TRUE) {
         tbl <- tbl_name |>
             gtsummary::tbl_summary(by = {{ by }}, label = label) |>
@@ -48,9 +62,27 @@ tabler <- function(tbl_name,
             gt::tab_header(title = title) |>
             gt::tab_source_note(source_note = source_note)
         
+    } 
+    
+    # Binary Logistic ----
+    else if (isBinary == TRUE) {
+        tbl <- tbl_name |>
+        gtsummary::as_gt() |>
+        gt::tab_options(
+            table.background.color = bg_color,
+            table.font.color = font_color,
+            table_body.vlines.color = vline_color,
+            table_body.hlines.color = hline_color,
+            column_labels.hidden = hide_column_labels
+        ) |>
+            gt::tab_header(title = title) |>
+            gt::tab_source_note(source_note = source_note)
+        
     } else {
         
-        tbl <- gt::gt(tbl_name) |>
+        # Ungrouped ----
+         tbl <- tbl_name |>
+            gt::gt() |>
             gt::tab_header(title = title) |>
             gt::fmt_number(decimals = decimals, columns = value_columns) |>
             gt::tab_options(
@@ -59,10 +91,12 @@ tabler <- function(tbl_name,
                 table_body.vlines.color = vline_color,
                 table_body.hlines.color = hline_color,
                 column_labels.hidden = hide_column_labels
-            )       |>
-            gt::tab_source_note(source_note = source_note)
-        
-    }
+                
+            ) |>
+             gt::tab_source_note(source_note = source_note) |>
+             gt::tab_footnote(footnote = {{ footnote }},
+                              location = gt::cells_column_labels(columns = {{ location }}))
+         }
     
     return(tbl)
 }
