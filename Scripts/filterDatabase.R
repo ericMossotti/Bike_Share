3f52b8897536985e03d88af0f199d8a3ac320640
Eric Mossotti <ecmossotti@gmail.com>
2024-05-20 23:51:16 -0500

    Add code documentation and clean up a bit.

--- Scripts/filterDatabase.R
+++ Scripts/filterDatabase.R
@@ -1,10 +1,14 @@
 filterDatabase <- function(conxn = dbconn,
                            path1 = dupelessPath,
                            path2 = tblPath_fltrd) {
-    dplyr::tbl(conxn, path1, check_from = FALSE) |>
-        dplyr::filter(trip_time > 1, trip_time < 480, rideable_type != "docked_bike") |>
+    dplyr::tbl(conxn, 
+               path1) |>
+        dplyr::filter(trip_time > 1, 
+                      trip_time < 480, 
+                      rideable_type != "docked_bike") |>
         dplyr::collect() |>
-        # Might as well calculate distance traveled while at it.
+        
+# Might as well calculate distance traveled while at it.
         dplyr::mutate(
             miles = geosphere::distGeo(
                 p1 = cbind(start_lng, start_lat),
@@ -12,11 +16,11 @@ filterDatabase <- function(conxn = dbconn,
             ) / 1000 * 0.62137119,
             mph = (miles / (trip_time / 60))
         ) |>
-        # It's nonsensical to rent a bike for distances easily walked.
-        dplyr::filter(miles > 0.1, # Seems that pro cyclists average around 20 mph,
-                      # so I set that as the ceiling.
-                      mph <= 20, # To account for time spent idling, stoplights and
-                      # traffic.
+# Floor rationale -  less than 0.1 miles are distances easily walked
+# Speed ceiling rationale - pro cyclists average around 20 mph
+# Speed floor rationale - accounts for trips possibly spent idling
+        dplyr::filter(miles > 0.1, 
+                      mph <= 20, 
                       mph > 1) |>
         duckdb::dbWriteTable(
             conn = conxn,
