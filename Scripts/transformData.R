e483a8260699692528e14b24cc0e9b793dc6547d
Eric Mossotti <ecmossotti@gmail.com>
2024-08-29 19:39:37 -0500

    Update documentation

--- Scripts/transformData.R
+++ Scripts/transformData.R
@@ -1,11 +1,15 @@
 
-# Programmatic manipulation of dataframe columns, utilizing tidy evaluation.
+# Programmatic db extraction and df transformation, utilizing tidy evaluation.
 #
 # Allows one to specify which columns to select, 
 # which columns to group by for counting, 
 # and which column (if any) to transform.
 #  
 # Can be used for a variety of similar data transformation tasks.
+# 
+# Results in weighted data that can help certain modeling functions
+# run faster while not altering the results one would expect
+# from using non-aggregated datasets as input.
 
 # function ---- 
 transformData <- function(dbconn,
@@ -23,17 +27,17 @@ transformData <- function(dbconn,
         dplyr::arrange(dplyr::across(dplyr::all_of(group_cols))) |>
         dplyr::collect()
     
-    # Perhaps overboard, but ensures the transform
+    # Perhaps overboard, but ensures the column to be transformed to binary exists
     if (!is.null(binary_col) &&
         binary_col %in% names(freq_data)) {
         freq_data <- freq_data |>
             dplyr::mutate(
-                # tidy select data with ':=' and bang-bang operators
+                # Tidy-selects data with ':=' and bang-bang operators
                 !!binary_col := dplyr::case_when(
-                    # Explicitly define as integers
+                    # Explicitly define values as integers
                     .data[[binary_col]] == zero_val ~ 0L,
                     .data[[binary_col]] == one_val ~ 1L,
-                    # assign NA to non-matching data
+                    # Assign NA to non-matching data
                     TRUE ~ NA_real_
                 ),!!binary_col := forcats::as_factor(.data[[binary_col]])
             )
