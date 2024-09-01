71236f61d4faed84ccb03b012a13ab300dac3a93
Eric Mossotti <ecmossotti@gmail.com>
2024-09-01 04:39:10 -0500

    Simplified binary transformation section, added new factor column with some useful quartile labels

--- Scripts/transformData.R
+++ Scripts/transformData.R
@@ -27,14 +27,23 @@ transformData <- function(dbconn,
                           ntile_col = NULL,
                           pred_col = NULL,
                           zero_val = NULL,
-                          one_val = NULL) {
+                          one_val = NULL,
+                          qtile_levels = NULL,
+                          doWeights = FALSE) {
     
-    freq_data <- dplyr::tbl(dbconn, tblPath_fltrd) |>
-        dplyr::select(dplyr::all_of(select_cols)) |>
-        dplyr::add_count(dplyr::across(dplyr::all_of(group_cols))) |>
-        dplyr::distinct() |>
-        dplyr::arrange(dplyr::across(dplyr::all_of(group_cols))) |>
-        dplyr::collect()
+    if (isTRUE(doWeights)) {
+        freq_data <- dplyr::tbl(dbconn, tblPath_fltrd) |>
+            dplyr::select(dplyr::all_of(select_cols)) |>
+            dplyr::add_count(dplyr::across(dplyr::all_of(group_cols))) |>
+            dplyr::distinct() |>
+            dplyr::arrange(dplyr::across(dplyr::all_of(group_cols))) |>
+            dplyr::collect()
+    } else {
+        freq_data <- dplyr::tbl(dbconn, tblPath_fltrd) |>
+            dplyr::select(dplyr::all_of(select_cols)) |>
+            dplyr::arrange(dplyr::across(dplyr::all_of(select_cols))) |>
+            dplyr::collect()
+    }
     
     # Perhaps overboard, but ensures the column to be transformed to binary exists
     if (!is.null(binary_col) &&
@@ -42,13 +51,8 @@ transformData <- function(dbconn,
         freq_data <- freq_data |>
             dplyr::mutate(
                 # Tidy-selects data with ':=' and bang-bang operators
-                !!binary_col := dplyr::case_when(
-                    # Explicitly define values as integers
-                    .data[[binary_col]] == zero_val ~ 0L,
-                    .data[[binary_col]] == one_val ~ 1L,
-                    # Assign NA to non-matching data
-                    TRUE ~ NA_real_),
-                !!binary_col := forcats::as_factor(.data[[binary_col]])
+                !!binary_col := factor(.data[[binary_col]],
+                                       levels = c(zero_val, one_val))
             )
     }
     
@@ -57,7 +61,9 @@ transformData <- function(dbconn,
         freq_data <- freq_data |>
             dplyr::mutate(
                 !!ntile_col := dplyr::ntile(.data[[pred_col]], n = 4),
-                !!ntile_col := forcats::as_factor(.data[[ntile_col]])
+                !!ntile_col := factor(.data[[ntile_col]],
+                                      levels = c(1, 2, 3, 4),
+                                      labels = as.vector(qtile_levels))
             )
     }
     
