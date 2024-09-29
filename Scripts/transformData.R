7ee15f7faf8a93ff75dc51e2faaffc08967d18cc
Eric Mossotti <ecmossotti@gmail.com>
2024-09-29 02:48:16 -0500

    Update header and documentation; add option to handle dataframes or databases; add some comments

--- Scripts/transformData.R
+++ Scripts/transformData.R
@@ -1,51 +1,61 @@
-# Header ----
 # Author: Eric Mossotti
-# Date: `r paste(Sys.Date())`
-# Copyright (c) Eric Mossotti, `r paste(format(Sys.Date(), "%Y"))`
-# Email: ecmossotti@gmail.com 
-
-# Description ----
-# 
+# CC BY-SA
+# ----
 # Programmatic db extraction and df transformation, utilizing tidy evaluation.
 #
-# Allows one to specify which columns to select, 
-# which columns to group by for counting, 
-# and which column (if any) to transform.
-#  
-# Can be used for a variety of similar data transformation tasks.
+# Allows one to specify which columns to select, which columns to group by for 
+# counting, and which column (if any) to transform. It be used for a variety of 
+# similar data transformation tasks.
 # 
-# Results in weighted data that can help certain modeling functions
-# run faster while not altering the results one would expect
-# from using non-aggregated datasets as input.
-
-# function ---- 
-transformData <- function(dbconn,
-                          tblPath_fltrd,
+# Results can be weighted to help certain modeling functions run faster while 
+# not altering the results one would expect from using non-aggregated datasets 
+# as input.
+# ---- 
+transformData <- function(conn,
+                          path,
+                          df,
                           select_cols,
-                          group_cols,
+                          group_cols = NULL,
                           binary_col = NULL,
                           ntile_col = NULL,
                           pred_col = NULL,
                           zero_val = NULL,
                           one_val = NULL,
                           qtile_levels = NULL,
-                          doWeights = FALSE) {
-    
+                          doWeights = FALSE,
+                          doQuantile = FALSE,
+                          isDF = FALSE) {
+    # Weight the data? ----
     if (isTRUE(doWeights)) {
-        freq_data <- dplyr::tbl(dbconn, tblPath_fltrd) |>
-            dplyr::select(dplyr::all_of(select_cols)) |>
-            dplyr::add_count(dplyr::across(dplyr::all_of(group_cols))) |>
-            dplyr::distinct() |>
-            dplyr::arrange(dplyr::across(dplyr::all_of(group_cols))) |>
-            dplyr::collect()
+        # Is the data from a df or db? ----
+        if (isTRUE(isDF)) {
+            freq_data <- df |>
+                dplyr::select(dplyr::all_of(select_cols)) |>
+                dplyr::add_count(dplyr::across(dplyr::all_of(group_cols))) |>
+                dplyr::distinct() |>
+                dplyr::arrange(dplyr::across(dplyr::all_of(group_cols)))
+        } else {
+            freq_data <- dplyr::tbl(conn, path) |>
+                dplyr::select(dplyr::all_of(select_cols)) |>
+                dplyr::add_count(dplyr::across(dplyr::all_of(group_cols))) |>
+                dplyr::distinct() |>
+                dplyr::arrange(dplyr::across(dplyr::all_of(group_cols))) |>
+                dplyr::collect()
+        }
     } else {
-        freq_data <- dplyr::tbl(dbconn, tblPath_fltrd) |>
-            dplyr::select(dplyr::all_of(select_cols)) |>
-            dplyr::arrange(dplyr::across(dplyr::all_of(select_cols))) |>
-            dplyr::collect()
+        # Is the data from a df or db? ----
+        if (isTRUE(isDF)) {
+            freq_data <- df |>
+                dplyr::select(dplyr::all_of(!!select_cols)) |>
+                dplyr::arrange(dplyr::across(dplyr::all_of(!!select_cols)))
+        } else {
+            freq_data <- dplyr::tbl(conn, path) |>
+                dplyr::select(dplyr::all_of(select_cols)) |>
+                dplyr::arrange(dplyr::across(dplyr::all_of(select_cols))) |>
+                dplyr::collect()
+        }
     }
-    
-    # Perhaps overboard, but ensures the column to be transformed to binary exists
+    # Do we want to transform a column to binary for modeling?
     if (!is.null(binary_col) &&
         binary_col %in% names(freq_data)) {
         freq_data <- freq_data |>
@@ -55,9 +65,8 @@ transformData <- function(dbconn,
                                        levels = c(zero_val, one_val))
             )
     }
-    
-    if (!is.null(pred_col) && 
-        pred_col %in% names(freq_data)) {
+    # Do we want to predict anything?
+    if (isTRUE(doQuantile)) {
         freq_data <- freq_data |>
             dplyr::mutate(
                 !!ntile_col := dplyr::ntile(.data[[pred_col]], n = 4),
