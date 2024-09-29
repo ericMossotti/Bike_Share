77e0ec73d551e5d630e76d3a7eaba14c9121faf2
Eric Mossotti <ecmossotti@gmail.com>
2024-09-29 02:53:01 -0500

    Update header; add description; rename variables

--- Scripts/unz_relocate.R
+++ Scripts/unz_relocate.R
@@ -1,10 +1,19 @@
-unz_relocate <- function (x = tempfile_paths,
-                          y = tempZipPaths,
-                          z = fileNames) {
-    for (i in seq(x)) {
-        utils::unzip(y[i],
-                     z[i])
-        file.rename(z[i],
-                    x[i])
+# ----
+# Author: Eric Mossotti
+# ----
+# Helpful because this unzips only the desired files contained inside compressed 
+# folders. The unzipped files are then relocated to user defined file paths.
+# 
+# There might well be existing libraries and methods in R that can do this, 
+# but I was not able to readily locate them. 
+# ----
+unz_relocate <- function (fPaths = tempfile_paths,
+                          zPaths = tempZipPaths,
+                          fNames = fileNames) {
+    for (i in seq(fPaths)) {
+        utils::unzip(zPaths[i],
+                     fNames[i])
+        file.rename(fNames[i],
+                    fPaths[i])
     }
 }
