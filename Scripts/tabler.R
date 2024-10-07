697b45613ff9f819fdf73fb40f73e3e01730d1d7
Eric Mossotti <ecmossotti@gmail.com>
2024-10-07 10:51:46 -0500

    remove rounding to decimal numbers; update stub notes

--- Scripts/tabler.R
+++ Scripts/tabler.R
@@ -32,11 +32,14 @@ tabler <- function(tbl_name,
                    chi_result = NULL,
                    chiVar = NULL,
                    chiBy = NULL,
-                   stub_label = NULL, 
+                   stub_label = NULL,
                    stub_note = NULL,
                    stubName = NULL,
-                   isStub = FALSE) {
-    
+                   isStub = FALSE,
+                   row_index = NULL,
+                   label_parameter = NULL,
+                   align_parameter = "center"
+                   ) {
     # Need to account for grouped tables and non-grouped tables
     # Grouped ----
     if (!is.null(groupName)) {
@@ -45,16 +48,23 @@ tabler <- function(tbl_name,
                       row_group_as_column = TRUE) |>
             gt::tab_header(title = title, subtitle = subtitle) |>
             gt::tab_stubhead(label = stub_label) |>
-            gt::fmt_number(decimals = decimals, columns = value_columns) |>
+            #gt::fmt_number(decimals = decimals, columns = value_columns) |>
             gt::tab_options(
                 table.background.color = bg_color,
                 table.font.color = font_color,
-                table_body.vlines.color = vline_color,
+                table.margin.left = 0,
+                table.margin.right = 0,
+                #table.border.right.color = "ivory",
+                #table.border.left.style = "ivory",
+                #table.border.top.color = "ivory",
+                #table.border.bottom.color = "ivory",
+                table_body.vlines.color = "transparent",
                 table_body.hlines.color = hline_color,
                 row.striping.include_table_body = TRUE,
                 row.striping.background_color = "gray10",
                 column_labels.font.weight = "bold",
-                column_labels.border.bottom.color = hline_color
+                #column_labels.border.bottom.color = hline_color,
+                column_labels.hidden = hide_column_labels
             ) |>
             gt::tab_source_note(source_note = {{ source_note }})
     }
@@ -62,14 +72,15 @@ tabler <- function(tbl_name,
     # Chi Square ----
     if (isSummary == TRUE) {
         tbl <- tbl_name |>
-            gtsummary::tbl_summary(by = {{ by }}, 
-                                   label = label) |>
+            gtsummary::tbl_summary(by = {{ by }}, label = label) |>
             gtsummary::modify_footnote(
                 gtsummary::all_stat_cols() ~ paste0(
-                    "n (%);   χ²  = ", 
+                    "n (%);   χ²  = ",
                     round(chi_result$statistic, 2),
-                    ";   df = ", 
-                    chi_result$parameter)) |>  
+                    ";   df = ",
+                    chi_result$parameter
+                )
+            ) |>
             gtsummary::add_p() |>
             gtsummary::as_gt() |>
             gt::tab_header(title = title, subtitle = subtitle) |>
@@ -85,11 +96,7 @@ tabler <- function(tbl_name,
                 locations = gt::cells_body(gt::everything())
             ) |>
             gt::tab_style(
-                gt::cell_text(
-                    align = "center",
-                    stretch = "semi-expanded",
-                    whitespace = "break-spaces"
-                ),
+                gt::cell_text(align = "center", stretch = "semi-expanded"),
                 locations = list(
                     gt::cells_title(groups = c("title", "subtitle")),
                     gt::cells_column_labels(gt::everything())
@@ -116,25 +123,17 @@ tabler <- function(tbl_name,
             gt::tab_style(
                 style = list(
                     gt::cell_borders(sides = "bottom"),
-                    gt::cell_text(
-                        align = "left",
-                        stretch = "semi-expanded",
-                     #   whitespace = "break-spaces"
-                    )
+                    gt::cell_text(align = "left", stretch = "semi-expanded")
                 ),
                 locations = gt::cells_body(gt::everything())
             ) |>
             gt::tab_style(
-                gt::cell_text(
-                    align = "center",
-                    stretch = "semi-expanded",
-                    #whitespace = "break-spaces"
-                ),
+                gt::cell_text(align = "center", stretch = "semi-expanded"),
                 locations = list(
                     gt::cells_title(groups = c("title", "subtitle")),
                     gt::cells_column_labels(gt::everything())
                 )
-            ) |> 
+            ) |>
             gt::tab_options(
                 table.background.color = bg_color,
                 table.font.color = font_color,
@@ -142,6 +141,7 @@ tabler <- function(tbl_name,
                 table_body.hlines.color = hline_color,
                 row.striping.include_table_body = TRUE,
                 row.striping.background_color = "gray10",
+                column_labels.hidden = hide_column_labels,
                 column_labels.font.weight = "bold",
             ) |>
             gt::tab_source_note(source_note = source_note)
@@ -153,65 +153,119 @@ tabler <- function(tbl_name,
         tbl <- tbl_name |>
             gt::gt() |>
             gt::tab_header(title = title, subtitle = subtitle) |>
-            gt::fmt_number(decimals = decimals, columns = value_columns) |>
+            #gt::fmt_number(decimals = decimals, columns = value_columns) |>
             gt::tab_style(
                 style = list(
                     gt::cell_borders(sides = c("right", "left"), color = vline_color),
-                    gt::cell_text(stretch = "semi-expanded", whitespace = "break-spaces")
+                    gt::cell_text(stretch = "semi-expanded")
                 ),
                 locations = gt::cells_body(gt::everything())
             ) |>
             gt::tab_style(
                 style = list(
-                    gt::cell_borders(sides = c("right", "left", "top"), color = vline_color),
-                    gt::cell_text(
-                        align = "center",
-                        stretch = "semi-expanded",
-                        whitespace = "break-spaces"
-                    )
+                    gt::cell_borders(
+                        sides = c("right", "left", "top"),
+                        color = vline_color
+                    ),
+                    gt::cell_text(align = "center", stretch = "semi-expanded")
                 ),
                 locations = list(
-                    gt::cells_title(groups = c("title", "subtitle")),
-                    gt::cells_column_labels(gt::everything())
+                    gt::cells_column_labels(gt::everything()),
+                    gt::cells_title(groups = c("title", "subtitle"))
                 )
-            ) |> 
+            ) |>
+            gt::tab_style(
+                style = list(
+                    gt::cell_text(align = align_parameter, stretch = "semi-expanded")
+                ),
+                locations = list(gt::cells_column_labels(columns = label_parameter))
+            )
+        # Need to avoid this part for non-frequency tables?
+        if (!is.null(label_n)) {
+            tbl <- tbl |> gt::tab_style(
+                style = list(
+                    gt::cell_text(align = "right", stretch = "semi-expanded")
+                ),
+                locations = list(gt::cells_column_labels(columns = label_n))
+            )
+        }
+        tbl <- tbl |>
             gt::tab_options(
+                #table_body.border.top.width = gt::px(0.15),
+               # table_body.border.bottom.width = gt::px(0.15),
                 table.background.color = bg_color,
                 table.font.color = font_color,
-                table_body.vlines.color = vline_color,
-                table_body.hlines.color = hline_color,
+                table.margin.left = 0,
+                table.margin.right = 0,
+                #table.margin.top = 0,
+                #table.margin.bottom = 0,
+                #table.border.right.color = "ivory",
+                #table.border.left.color = "ivory",
+                #table.border.top.color = "ivory",
+                #table.border.bottom.color = "ivory",
+                #table.border.left.width = gt::px(0.15),
+                #table.border.right.width = gt::px(0.15),
+                #table.border.top.width = gt::px(0.15),
+                #table.border.bottom.width = gt::px(0.15),
+                table_body.vlines.color = "transparent",
+                table_body.hlines.color = "transparent",
                 column_labels.vlines.color = vline_color,
+                column_labels.hidden = hide_column_labels,
+                #column_labels.padding = gt::px(1),
                 heading.border.lr.color = "transparent",
-                table.border.bottom.color = "transparent",
-                table.border.top.color = "transparent",
+                #heading.padding = gt::px(10),
+                #column_labels.border.lr.color = "ivory",
+                container.padding.x = 0,
+                container.padding.y = 0,
+                #  table.border.bottom.color = "transparent",
+                #  table.border.top.color = "transparent",
+                #column_labels.border.top.width = gt::px(0.15),
+                #column_labels.border.bottom.width = gt::px(0.15),
                 row.striping.include_table_body = TRUE,
                 row.striping.background_color = "gray10",
-                column_labels.font.weight = "bold"
+                column_labels.font.weight = "bold",
+                #footnotes.border.lr.color = "ivory",
+               # footnotes.border.lr.width = gt::px(0.15),
+               #ihtml.active = TRUE,
+               #ihtml.use_highlight = TRUE,
+               #ihtml.use_compact_mode = TRUE,
+               #ihtml.use_sorting = TRUE,
+               #ihtml.use_resizers = TRUE,
+               #ihtml.use_search = TRUE
             ) |>
             gt::tab_source_note(source_note = source_note)
     }
     # To add footnotes to the various table types ----
     if (!is.null(note_list) && !is.null(location_list)) {
         tbl <- tbl |>
-            add_multiple_footnotes(note_list, location_list, noteColumns, noteRows)
+            add_multiple_footnotes(note_list,
+                                   location_list,
+                                   noteColumns,
+                                   noteRows,
+                                   row_index)
     }
     
     if (isTRUE(isStub)) {
         tbl <- tbl |>
             gt::tab_stubhead(label = stub_label) |>
             gt::cols_align(columns = groupName, align = "center") |>
-            gt::tab_footnote(footnote = stub_note,
-                             location = gt::cells_stubhead(),
-                             placement = "left")
+            gt::tab_footnote(
+                footnote = stub_note,
+                location = gt::cells_stubhead(),
+                placement = "auto"
+            )
     }
-    
     return(tbl)
 }
 
 
 # Footnotes function ----
-add_multiple_footnotes <- function(tbl, note_list, location_list, noteColumns, noteRows) {
-    
+add_multiple_footnotes <- function(tbl,
+                                   note_list,
+                                   location_list,
+                                   noteColumns,
+                                   noteRows,
+                                   row_index) {
     if (length(note_list) != length(location_list)) {
         stop("The lengths of note_list and location_list must be equal.")
     }
@@ -230,13 +284,13 @@ add_multiple_footnotes <- function(tbl, note_list, location_list, noteColumns, n
     if (isTRUE(noteRows)) {
         for (i in seq_along(note_list)) {
             tbl <- tbl |>
-                gt::tab_footnote(footnote = note_list[[i]],
-                                 location = gt::cells_stub(rows = location_list[[i]]),
-                                 placement = "left"
-                ) 
+                gt::tab_footnote(
+                    footnote = note_list[[i]],
+                    location = gt::cells_body(columns = location_list[[i]], rows = row_index),
+                    placement = "auto"
+                )
         }
     }
     
     return(tbl)
 }
-
